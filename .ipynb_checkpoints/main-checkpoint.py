6cce1456d94253d1bb747b92134ceafb4fd69754
Ousama Medebber <60102334@udst.edu.qa>
2025-03-19 11:26:10 +0300

    updated version of celery

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,5 +1,6 @@
-from tasks import power
-from dispatch_tasks import dispatch
+
+from src.tasks import power
+from src.dispatch_tasks import dispatch
 
 if __name__ == "__main__":
     results = dispatch()
